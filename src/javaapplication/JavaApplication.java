989f3971fa5e1d8058a14b9dbae8d25eebac1eed
leNigaud <rafeno231@gmail.com>
2023-06-15 15:06:40 +0300

    initial commit

---
+++ src/javaapplication/JavaApplication.java
@@ -0,0 +1,18 @@
+
+package javaapplication;
+
+/**
+ *
+ * @author J I N
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("hello world");
+    }
+    
+}
